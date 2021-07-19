2fb13282abd8e029ef6899306271c3928aae44db
Shakir <mshakir628@gmail.com>
2021-07-19 13:21:52 +0500

    Initial Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FormsModule } from '@angular/forms';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-courses.ts
@@ -0,0 +1,12 @@
+import { Course } from "./course";
+
+export const COURSES: Course[] = [
+    {id: 1, name: 'C++'},
+    {id: 2, name: 'Python'},
+    {id: 3, name: 'Angular'},
+    {id: 4, name: 'Java'},
+    {id: 5, name: 'DevOps'},
+    {id: 6, name: 'C#'},
+    {id: 7, name: 'CyberSecurity'},
+    {id: 8, name: 'Machine Learning'},
+]
